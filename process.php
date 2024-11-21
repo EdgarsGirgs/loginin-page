0ee397fdb2b897706fb3e1ebb824a45c3a414451
edgars Girgensons <you@edgars.com>
2024-11-21 13:34:38 +0200

    try and catch has been added

--- process.php
+++ process.php
@@ -5,4 +5,10 @@ $dbname = 'user_database';
 $username = 'root';  
 $password = ''; 
 
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
 ?>
